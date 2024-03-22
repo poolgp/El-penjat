5cb50c3620b40a3e48470980c27323997bf4693c
poolgp <145465364+poolgp@users.noreply.github.com>
2024-03-22 16:46:01 +0100

    22/03/2024

--- index.js
+++ index.js
@@ -92,11 +92,12 @@ function mostrarPalabra() {
 
 function mostrarTematica() {
   let tematicaSpan = document.getElementById("tema");
-  console.log(palabraSeleccionada.tematica);
+  // console.log(palabraSeleccionada.tematica);
   tematicaSpan.textContent = palabraSeleccionada.tematica;
 }
 
 function ocultar() {
+  //ocultar Form i imgs
   document.getElementById("idForm").style.visibility = "hidden";
 
   const elementosOcultar = document.getElementsByClassName("ocultar");
@@ -104,16 +105,6 @@ function ocultar() {
     elementosOcultar[i].style.visibility = "hidden";
   }
 }
-
-document.addEventListener("keydown", function (event) {
-  if (/^[a-zA-Z]$/.test(event.key)) {
-    const letraPresionada = event.key.toUpperCase();
-
-    comprobarLetraPorTeclado(letraPresionada);
-
-    event.preventDefault();
-  }
-});
 
 function mostrarAbc() {
   abcSeparado = abc.split("");
@@ -135,19 +126,10 @@ function mostrarAbc() {
     aElement.id = `caracter-${caracter}`;
     aElement.textContent = caracter;
 
-    const comprobarLetraEvento = function (event) {
+    aElement.onclick = function (event) {
       comprobarLetra(event);
-      aElement.removeEventListener("click", comprobarLetraEvento);
+      aElement.onclick = null;
     };
-
-    aElement.onclick = comprobarLetraEvento;
-
-    document.addEventListener("keydown", function (event) {
-      if (/^[a-zA-Z]$/.test(event.key) && event.key.toUpperCase() === caracter) {
-        comprobarLetraPorTeclado(caracter);
-        aElement.removeEventListener("click", comprobarLetraEvento);
-      }
-    });
 
     if (i < 10) {
       abc1.appendChild(aElement);
@@ -157,7 +139,6 @@ function mostrarAbc() {
       abc3.appendChild(aElement);
     }
   });
-
 
   container.appendChild(abc1);
   container.appendChild(abc2);
@@ -180,20 +161,20 @@ function comprobarLetra(event) {
   }
 }
 
-function comprobarLetraPorTeclado(letra) {
-  let letraElement = document.getElementById(`caracter-${letra}`);
+// function comprobarLetraPorTeclado(letra) {
+//   let letraElement = document.getElementById(`caracter-${letra}`);
 
-  if (arrayPalabra.includes(letra)) {
-    mostrarLetra(letra);
-    letraElement.style.color = "green";
-    letraElement.removeEventListener("click", comprobarLetraPorTeclado);
-  } else {
-    numErrores++;
-    letraElement.style.color = "red";
-    actualitzarContadorErrores();
-    letraElement.removeEventListener("click", comprobarLetraPorTeclado);
-  }
-}
+//   if (arrayPalabra.includes(letra)) {
+//     mostrarLetra(letra);
+//     letraElement.style.color = "green";
+//     letraElement.removeEventListener("click", comprobarLetraPorTeclado);
+//   } else {
+//     numErrores++;
+//     letraElement.style.color = "red";
+//     actualitzarContadorErrores();
+//     letraElement.removeEventListener("click", comprobarLetraPorTeclado);
+//   }
+// }
 
 function mostrarLetra(letraClick) {
   for (let i = 0; i < arrayPalabra.length; i++) {
@@ -215,8 +196,6 @@ function actualitzarContadorErrores() {
 function palabraCompleta() {
   elUsuarioCompletoLaPalabra = false;
   const completa = document.getElementById("palabra").textContent;
-  /*console.log(typeof(palabraSeleccionada.nombre));
-  console.log(typeof(completa));*/
 
   if (palabraSeleccionada.nombre === completa) {
     finJoc();
@@ -267,54 +246,57 @@ function finJoc() {
 }
 
 function tornarJugar() {
-
-  location.reload();
+  reiniciar();
+  mostrarNombre();
+  selectParaula();
+  mostrarPalabra();
+  mostrarTematica();
+  ocultar();
+  mostrarAbc();
 }
 
-// let palabras = [
-//   {
-//     nombre: "ESPANYA",
-//     imagen: "./img/SagradaFamilia.jpg",
-//     descripcion:
-//       "Espanya, situada a la península ibèrica del sud-oest d'Europa, és un país divers i ric en història i cultura. Les seves encisadores ciutats, platges banyades pel sol i paisatges pintorescs de muntanya atreuen visitants de tot el món. Amb una barreja de tradició i modernitat, Espanya és coneguda per la seva arquitectura impressionant, la seva cuina deliciosa, la seva vibrante vida nocturna i els festivals colorits. Des dels majestuosos palauets fins a les bullicioses places, Espanya ofereix una experiència única que fusiona la passió i l'alegria en un entorn fascinant.",
-//     tematica: "Nom d'un país",
-//   },
-//   {
-//     nombre: "FRANCA",
-//     imagen: "./img/TorreEiffel.jpg",
-//     descripcion:
-//       "França, al cor d'Europa occidental, és coneguda per la seva rica història, el seu art excepcional, la seva exquisida gastronomia i la seva diversitat geogràfica. Des dels encisadors paisatges de la Provença fins a la majestuositat dels Alps, França ofereix una barreja única de tradició i modernitat. Les seves ciutats, com París amb la Torre Eiffel, reflecteixen l'elegància i la sofisticació, mentre que els seus poblets pintorescos i el seu patrimoni cultural contribueixen al seu atractiu atemporal. Amb una influència duradora en la moda, la literatura i la filosofia, França continua sent un destí culturalment ric i captivador.",
-//     tematica: "Nom d'un país",
-//   },
-//   {
-//     nombre: "LLEO",
-//     imagen: "./img/Leon.jpg",
-//     descripcion:
-//       "El lleó és un majestuós felí que es distingeix per la seva melena en el cas dels mascles. Conegut com el 'rei de la selva', habita principalment a l'Àfrica subsahariana i en algunes àrees d'Àsia. Els lleons són animals socials que viuen en grups anomenats manades, liderades per un mascle dominant. La seva imponent presència i el seu distintiu rugit han contribuït al seu estatus com a símbol de força i noblesa en diverses cultures i mitologies.",
-//     tematica: "Animals del món",
-//   },
-//   {
-//     nombre: "KOALA",
-//     imagen: "./img/koala.jpg",
-//     descripcion:
-//       "El koala és un adorabl marsupial originari d'Austràlia, conegut pel seu pelatge suau i gris, grans orelles rodones i nasal prominente. La seva dieta consisteix principalment en fulles d'eucaliptus, i són animals arboris que passen la major part del seu temps descansant a les branques. Tot i que la seva aparença tendra i el seu comportament tranquil els fan estimats, els koales s'enfronten a amenaces en termes de conservació a causa de la pèrdua d'hàbitat i altres reptes ambientals.",
-//     tematica: "Animals del món",
-//   },
-//   {
-//     nombre: "COTXE",
-//     imagen: "./img/coche.jpg",
-//     descripcion:
-//       "El cotxe, o automòbil, és un mitjà de transport personal amplament utilitzat. Disposa de rodes i motor, oferint una manera còmoda i eficient de desplaçar-se. Els cotxes varien en mida i disseny, des de vehicles compactes fins a SUVs o cotxes esportius. Són una peça clau de la vida quotidiana, proporcionant mobilitat i comoditat a les persones en les seves rutines diàries.",
-//     tematica: "Vehicles de transport",
-//   },
-//   {
-//     nombre: "AVIO",
-//     imagen: "./img/avion.jpg",
-//     descripcion:
-//       "L'avió és una aeronau impulsada per motors que es desplaça a través de l'aire. Amb ales que proporcionen sustentació i motors per a la propulsió, els avions són utilitzats per al transport de passatgers, càrrega o per finalitats militars. Aquest mitjà de transport ha revolucionat les comunicacions a escala global, permetent viatges ràpids i eficients entre diferents regions del món.",
-//     tematica: "Vehicles de transport",
-//   },
-// ];
+function reiniciar() {
+  //mostrar valoes por defecto
+  // document.getElementById("idForm").style.display = "none";
+  // const imgFinal = document.querySelector(".imgFinal");
+  // imgFinal.style.visibility = "visible";
+
+  const finalImg = document.getElementById("imgFinal");
+  finalImg.style.visibility = "hidden";
+
+  const finalDescripcion = document.getElementById("descripcionFinal");
+  finalDescripcion.style.visibility = "hidden";
+
+  const palabraContainer = document.getElementById("palabra");
+  palabraContainer.style.visibility = "visible";
+
+  const abcContainer = document.getElementById("containerLletras");
+  abcContainer.style.visibility = "visible";
+
+  const contadorErrores = document.getElementById("contadorErrores");
+  contadorErrores.style.visibility = "visible";
+
+  const tornarJugar = document.getElementById("tornarJugar");
+  tornarJugar.style.visibility = "hidden";
+
+  // const titolFinal = document.getElementById("titolFinal");
+  // titolFinal.textContent = palabraSeleccionada.nombre;
+
+  // const descripcion = document.getElementById("descripcion");
+  // descripcion.textContent = palabraSeleccionada.descripcion;
+
+  // const tornarJugar = document.getElementById("tornarJugar");
+  // tornarJugar.style.visibility = "visible";
+}
+
+// function tornarJugar() {
+//   mostrarNombre();
+//   selectParaula();
+//   mostrarPalabra();
+//   mostrarTematica();
+//   ocultar();
+//   mostrarAbc();
+// }
 
 // let palabraSeleccionada;
 // let arrayPalabra;
@@ -415,7 +397,7 @@ function tornarJugar() {
 //   container.appendChild(abc1);
 //   container.appendChild(abc2);
 //   container.appendChild(abc3);
-// }
+// });
 
 // function comprobarLetra(event) {
 //   letraClick = event.target.textContent;
